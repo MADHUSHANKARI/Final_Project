a37af75b70f883d6051a6a147f9f3150eed79ee2
Thuva-hub <thuvaat1999@gmail.com>
2023-10-26 11:11:30 +0530

    Add existing file

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,61 +1,91 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
 import './Login.css'; // Import your login page styles here
+import { Formik, Form, Field, ErrorMessage } from 'formik';
+import { useNavigate } from 'react-router-dom';
+import * as Yup from 'yup';
+import axios from 'axios';
+import { ToastContainer, toast } from 'react-toastify';
+import 'bootstrap/dist/css/bootstrap.min.css'; // Import Bootstrap CSS
 
-function Login() {
-  // Define state for form fields
-  const [formData, setFormData] = useState({
-    email: '',
-    password: '',
+
+const Login = () => {
+  const navigate = useNavigate();
+
+  const LoginSchema = Yup.object().shape({
+    email: Yup.string().email('Invalid email').required('Email is required'),
+    password: Yup.string()
+      .min(8, 'Password must be at least 8 characters')
+      .matches(
+        /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).{8,}$/,
+        'Password must contain at least one uppercase letter, one lowercase letter, and one digit'
+      )
+      .required('Password is required'),
   });
 
-  // Handle form input changes
-  const handleInputChange = (e) => {
-    const { name, value } = e.target;
-    setFormData({
-      ...formData,
-      [name]: value,
-    });
-  };
-
-  // Handle form submission
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    // TODO: Add login authentication logic here
-    console.log('Form data submitted:', formData);
-  };
-
   return (
-    <div className="login-container">
-      <h2>Login</h2>
-      <form onSubmit={handleSubmit}>
-        <div className="form-group">
-          <input
-            type="email"
-            name="email"
-            value={formData.email}
-            onChange={handleInputChange}
-            placeholder="Email"
-            required
-          />
-        </div>
-        <div className="form-group">
-          <input
-            type="password"
-            name="password"
-            value={formData.password}
-            onChange={handleInputChange}
-            placeholder="Password"
-            required
-          />
-        </div>
-        <button type="submit">Login</button>
-      </form>
-      <p>
-        Don't have an account? <Link to="/signup">Sign Up</Link>
-      </p>
-    </div>
+    <div className='loginpage'>
+
+      <Formik
+        initialValues={{
+          email: '',
+          password: '',
+        }}
+        validationSchema={LoginSchema}
+        onSubmit={async (values, { resetForm }) => {
+          try {
+            const response = await axios.post('http://localhost:5000/v1/login', values);
+            console.log(response.data);
+            toast.success('Login successful', { autoClose: 3000 });
+            localStorage.setItem('valid', response.data);
+            localStorage.setItem('email', values.email);
+            navigate('/Dashboard');
+          } catch (error) {
+            console.error(error);
+            const errorMessage = error.response?.data || 'An error occurred';
+            toast.warning(errorMessage, { autoClose: 3000 });
+          }
+        }}
+      >
+        {({ errors, touched }) => (
+         
+          <Form>
+            <div className="login-form">
+              <div style={{ marginBottom: '25px' }}>
+                <Field
+                  name="email"
+                  type="email"
+                  placeholder="Email"
+                  style={{ width: '100%', padding: '0.5rem', backgroundColor: '#929692', fontSize: '18px', border: 'none' }}
+                />
+                <ErrorMessage name="email" component="div" className="error-message" />
+              </div>
+              <div style={{ marginBottom: '25px' }}>
+                <Field
+                  name="password"
+                  type="password"
+                  placeholder="Password"
+                  style={{ width: '100%', padding: '0.5rem', backgroundColor: '#929692', fontSize: '18px', border: 'none' }}
+                />
+                <ErrorMessage name="password" component="div" className="error-message" />
+              </div>
+              <button className="login-button" type="submit">
+                Login
+              </button>
+              <div className="forgot-password">
+                <a href="#">Forgot password?</a>
+              </div>
+              <Link to="/Signup" state={{}}>
+                <button className="create-account-button">Create new account</button>
+              </Link>
+            </div>
+          </Form>
+         
+        )}
+      </Formik>
+      </div>
+    
   );
-}
+};
 
 export default Login;
